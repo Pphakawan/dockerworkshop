e59c04c3c43092940eb75d6402bc26245aed4fab
nnutica <nitinat.loru@bumail.net>
2025-04-06 14:55:35 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+    <div className='text-3xl font-bold underline text-green-500'>
+        Docker Workshop
+    </div>
+    <div className='text-2xl font-bold text-blue-400'> Phakawan Raengthong 1650706979</div>
+    </div>
+  )
+}
+
+export default App
